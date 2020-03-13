aecc7dbc246ceabd0708773392072e8ee1e3de39
gunnarmein <gmein@eastsideprep.org>
2020-03-13 15:11:29 -0700

    async requests

--- MathServer/src/main/resources/script.js
+++ MathServer/src/main/resources/script.js
@@ -1,5 +1,21 @@
 console.log("Hello world!");
 
 
+function plus(a, b) {
+    request({url: "/plus?p1="+a+"&p2="+b, verb: "GET"})
+            .then(data => {
+                console.log("success: "+a+"+"+b+" is " + data);
+            })
+            .catch(error => {
+                console.log("error: " + error);
+            });
+    
+}
+
+for (var i=0; i<50; i++) {
+    plus(i, i);
+}
+
+
 
 
