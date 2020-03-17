09531ff326063f02256e85a25ad63108a216e712
Kenneth Y <kyang@eastsideprep.org>
2020-03-17 08:55:48 -0700

    CHATSERVER: can update messages from ArrayList

---
+++ ChatServer/src/main/java/org/eastsideprep/chatserver/Main.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eastsideprep.chatserver;
+
+import java.util.ArrayList;
+import static spark.Spark.*;
+
+// @Author: Kenneth Y.
+public class Main {
+
+    public static ArrayList<String> allMessagesArrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // tell spark where to find all the HTML and JS
+        staticFiles.location("/");
+        port(80);
+
+        // get a silly route up for testing
+        get("/hello", (req, res) -> {
+            System.out.println("Hey we were invoked:");
+            return "Hello world from code";
+        });
+
+        
+        // Manually Populate
+        allMessagesArrayList.add("Message 1");
+        allMessagesArrayList.add("Second Message");
+        allMessagesArrayList.add("Something else");
+        
+        
+        // get all new messages
+        get("/update_messages", (req, res) -> {
+            System.out.println("Update messages requested: ");
+
+            String msgs = "";
+
+//            allMessagesArrayList.add(req.session().id());
+            allMessagesArrayList.add(req.session().toString());
+
+            for (int i = 0;
+                    i < allMessagesArrayList.size();
+                    i++) {
+                String get = allMessagesArrayList.get(i);
+
+                msgs += get;
+                msgs += "\n";
+            }
+
+            return msgs;
+        });
+
+        // Subtract stuff
+        get("/minus", (req, res) -> {
+            System.out.println("Subtraction requested");
+
+            // Get's inputs as Strings from req
+            String p1 = req.queryParams("p1");
+            String p2 = req.queryParams("p2");
+
+            // Check if inputs are null
+            if (p1 == null || p2 == null) {
+                throw halt();
+            }
+
+            // Converts inputs to Double
+            double d1 = Double.parseDouble(p1);
+            double d2 = Double.parseDouble(p2);
+
+            // Return addition of doubles
+            return d1 - d2;
+        });
+
+    }
+
+}
---
+++ ChatServer/src/main/resources/script.js
@@ -0,0 +1,51 @@
+console.log("Hello world!");
+
+
+function plus(a, b, f) {
+    request({url: "/plus?p1="+a+"&p2="+b, verb: "GET"})
+            .then(data => {
+                console.log("success: "+a+"+"+b+" is " + data);
+                f(data);
+            })
+            .catch(error => {
+                console.log("error: " + error);
+            });
+    
+}
+
+function updateMessages(f) {
+    request({url: "/update_messages", verb: "GET"}).then(data => {
+        console.log("Success! Updating Messages");
+        f(data);
+    }).catch(error => {
+        console.log("Something borked: "+error);
+    });
+}
+function sendMessage(msg, f) {
+    console.log(msg);
+//    request({url: "/update_messages", verb: "GET"}).then(data => {
+//        console.log("Success! Updating Messages");
+//        f(data);
+//    }).catch(error => {
+//        console.log("Something borked: "+error);
+//    });
+}
+
+
+function plus_from_input() {
+    var p1 = document.getElementById("p1").value;
+    var p2 = document.getElementById("p2").value;
+    
+    plus(p1,p2,function (data) { document.getElementById("result").value = data; });
+}
+function updateMessagesTextArea() {
+    updateMessages(function (data) { document.getElementById("result").value = data; });
+}
+
+for (var i=0; i<50; i++) {
+    plus(i, i);
+}
+
+
+
+
