f63a12c1d0c5afaaddc5ad4e5b238d8938962bd3
surbhiijain <30298846+surbhiijain@users.noreply.github.com>
2020-03-25 11:36:45 -0700

    functional send and get routes

--- MathServer/src/main/java/org/eastsideprep/mathserver/Main.java
+++ MathServer/src/main/java/org/eastsideprep/mathserver/Main.java
@@ -8,11 +8,10 @@ package org.eastsideprep.mathserver;
 import static spark.Spark.*;
 import java.util.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
-        
+
         port(80);
 
         // tell spark where to find all the HTML and JS
@@ -33,54 +32,48 @@ public class Main {
             System.out.println("Addition requested :D");
             String p1 = req.queryParams("p1");
             String p2 = req.queryParams("p2");
-            
+
             if (p1 == null || p2 == null) {
                 throw halt();
             }
-            
+
             double d1 = Double.parseDouble(p1);
             double d2 = Double.parseDouble(p2);
-            
-            return d1+d2;
-        });
-        
 
-        get("/minus", (req,res) -> {
+            return d1 + d2;
+        });
+
+        get("/minus", (req, res) -> {
             System.out.println("Substraction requested");
             String p1 = req.queryParams("p1");
             String p2 = req.queryParams("p2");
-            
-            if(p1 == null || p2 == null) {
+
+            if (p1 == null || p2 == null) {
 
                 throw halt();
             }
-            
+
             double d1 = Double.parseDouble(p1);
             double d2 = Double.parseDouble(p2);
-            
-            return d1-d2;
+
+            return d1 - d2;
         });
 
+        ArrayList<String> messages = new ArrayList<String>();
+        messages.add("First Message");
+        messages.add("Second message");
 
-        ArrayList<String> messages = new ArrayList<String>();   
-       
-        get("/chat", (req,res) -> {     
-            String mes = "";
-            for (int i = 0; i < messages.size(); i++) {
-                mes += (messages.get(i));
-                mes.concat(",");
-            }
-            
-            return(mes);
+        get("/update_messages", (req, res) -> {
+            System.out.println("Get all messages requested");
+            String mes = String.join(" \n", messages);
+            return (mes);
         });
-        get("/send", (req,res) -> {  
+
+        put("/send", (req, res) -> {
+            System.out.println("Send message requested");
             String mess = req.queryParams("message");
             messages.add(mess);
-            return null;
-            
+            return mess;
         });
-        
-
     }
-
 }
