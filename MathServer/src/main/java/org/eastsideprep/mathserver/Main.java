542e1713c81d3de9c825913801dd86f53bd9f54e
EviyEmoji <43426819+EviyEmoji@users.noreply.github.com>
2020-04-29 14:42:14 -0700

    Created HanabiServerNB netbeans project
    but folder is still called mathserver

--- MathServer/src/main/java/org/eastsideprep/mathserver/Main.java
+++ MathServer/src/main/java/org/eastsideprep/mathserver/Main.java
@@ -10,14 +10,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import static spark.Spark.*;
 
+//HANABI SERVER NB
+
 public class Main {
 
     public static void main(String[] args) {
-
-        test();
-        if (true) {
-            return;
-        }
 
         port(80);
 
@@ -30,45 +27,5 @@ public class Main {
             return "Hello world from code";
         });
 
-        get("/tau", (req, res) -> {
-            System.out.println("Tau requested");
-            return 2 * Math.PI;
-        });
-
-        get("/plus", (req, res) -> {
-            System.out.println("Addition requested");
-            String p1 = req.queryParams("p1");
-            String p2 = req.queryParams("p2");
-
-            if (p1 == null || p2 == null) {
-                throw halt();
-            }
-
-            double d1 = Double.parseDouble(p1);
-            double d2 = Double.parseDouble(p2);
-
-            return d1 + d2;
-        });
-
-    }
-
-    spark.Session getSession(spark.Request req) {
-        spark.Session s = req.session(true); // if there is none, make one
-        return s;
-    }
-
-    String user(spark.Request req) {
-        String user = getSession(req).attribute("user");
-        if (user == null) {
-            user = "unknown"; // default name
-        }
-        return user;
-    }
-    
-    void login(spark.Request req) {
-        // take parameter from URL
-        String param = ""; // you need to replace this code
-        
-        getSession(req).attribute("user", param);
     }
 }
