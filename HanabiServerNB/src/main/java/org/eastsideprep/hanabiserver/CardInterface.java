a8bce98985faa6a3e11541b881c2497145fad59a
Kenneth Y <kyang@eastsideprep.org>
2020-05-01 12:27:15 -0700

    renamed package fro mathserver to hanabiserver, installed correct version of gson (2.80 -> 2.8.1)

--- HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/CardInterface.java
+++ HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/CardInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.eastsideprep.mathserver;
+package org.eastsideprep.hanabiserver;
 
 /**
  *
--- HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/PlayerInterface.java
+++ HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/PlayerInterface.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.eastsideprep.mathserver;
+package org.eastsideprep.hanabiserver;
 
 /**
  *
  * @author etardif
  */
-public interface HandInterface {
+public interface PlayerInterface {
     
 }
