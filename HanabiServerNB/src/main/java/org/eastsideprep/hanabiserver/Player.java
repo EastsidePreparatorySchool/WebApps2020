a9b4f4085e726fd5bd2885123efa50cd511fa37d
Kenneth Y <kyang@eastsideprep.org>
2020-05-05 15:26:17 -0700

    Player interface with basic implementation and added hint interface and class

--- HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Player.java
+++ HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Player.java
@@ -5,10 +5,63 @@
  */
 package org.eastsideprep.hanabiserver;
 
+import java.util.ArrayList;
+import org.eastsideprep.hanabiserver.interfaces.PlayerInterface;
+
 /**
  *
  * @author eoreizy
  */
-public class Player {
+public class Player implements PlayerInterface {
+
+    // Private Variables
+    private String myUsername;
+    private Hand myHand;
+    private ArrayList<Hint> myHints; // things I known about my cards
+
+    @Override
+    public void SendHint(Hint hint, Player toPlayer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void SendDiscard(Card card) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void SendMove(Card card) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void UpdateSelfHints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ReceiveHint(Hint hint) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void AddCardToHand(Card card) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String GetUsername() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Hand GetHand() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Hint> GetReceivedHints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
