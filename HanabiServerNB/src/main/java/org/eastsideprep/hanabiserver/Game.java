ffd3ba63379e74697801434f7b99f46ab7f64cf4
EviyEmoji <43426819+EviyEmoji@users.noreply.github.com>
2020-05-05 15:12:40 -0700

    worked on game interface

--- HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Game.java
+++ HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Game.java
@@ -5,10 +5,42 @@
  */
 package org.eastsideprep.hanabiserver;
 
+import java.util.ArrayList;
+import org.eastsideprep.hanabiserver.interfaces.GameInterface;
+
 /**
  *
  * @author eoreizy
  */
-public class Game {
+public class Game implements GameInterface {
+
+    private ArrayList<Player> players;
+    
+    private ArrayList<Card> deck;
+    
+    @Override
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    @Override
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    @Override
+    public Player getPlayerAtId(int id) {
+        return players.get(id);
+    }
+
+    @Override
+    public Player getPlayerHandAtId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //Until I see whatever the getHand() function on a Player is.
+    }
+
+    @Override
+    public Card getDeckCardAtId(int id) {
+        return deck.get(id);
+    }
     
 }
