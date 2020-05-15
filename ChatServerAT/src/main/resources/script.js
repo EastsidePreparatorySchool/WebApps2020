b066245c4b6b04d6c3f6c0ec6108a3f03c64e8ca
lightserpent <31710523+lightserpent@users.noreply.github.com>
2020-05-15 00:12:40 -0700

    Sprint 1 Work
    Made six piles in HTML for players, centered with good sizing and spacing. Added playerLabels too. Instead of an independent class object in javascript, I'm accommodating the Server group's card class object by making an updateCardInfo function, which takes in the new card parameters (color and number) and adds it to a specific card (also specified in parameters). Akso to test card updating, I made a randomizeCards function that does what you think, also made a display username to be used later for actual usernames. Everything from my original goal is completed.

--- ChatServerAT/src/main/resources/script.js
+++ ChatServerAT/src/main/resources/script.js
@@ -1,10 +1,31 @@
 console.log("Hello world!");
-
 logIn();
+function updateCardInfo(playerNumber, slotNumber, newColor, newNumber) {
+    //player number and slot number select what card is going to be changed, slot number is from left to right (1 for left, 2 is middle, etc)
+    var playerCard = document.getElementById("player" + playerNumber + "Card" + slotNumber);
+    playerCard.style.color = newColor;
+    playerCard.innerHTML = newNumber;
+}
+
+function RandomizeCards() { //function to randomize cards from 1-5 and colors wise
+    var colors = ["red", "blue", "purple", "green", "yellow"];
+    for (var i = 1; i < 7; i++) { //iterate thru players
+        for (var j = 1; j < 4; j++) { //iterate thru player slots
+            updateCardInfo(i, j, colors[Math.floor(Math.random() * colors.length)], Math.round((Math.random() * 4 + 1)));
+        }
+    }
+}
+
+function displayUsername(playerNumber, username){
+    document.getElementById("playerLabel"+playerNumber).innerHTML = username
+}
+
+
+
 
 function logIn() {
-   // var username = prompt("Please enter your username.");
-    request({url: "/login_user?username="+username, verb: "GET"})
+// var username = prompt("Please enter your username.");
+    request({url: "/login_user?username=" + username, verb: "GET"})
             .then(username => {
                 console.log(username);
                 document.getElementById("displayLogIn").innerHTML = "Logged in as " + username + ".";
@@ -15,9 +36,11 @@ function logIn() {
             });
 }
 
+
+
 function sendMsg() {
     var a = document.getElementById("msgBox").value;
-    request({url: "/send?msg="+a, verb: "PUT"})
+    request({url: "/send?msg=" + a, verb: "PUT"})
             .then(data => {
                 console.log(a);
             })
@@ -34,16 +57,14 @@ function getNew() {
                 // twice but I'm still unsure as to why it didn't just work by 
                 // doing it once the first time)
                 // here's what helped me solve the issue: 
-                    // https://stackoverflow.com/questions/30194562/json-parse-not-working
-                var messages = JSON.parse(JSON.parse(data)); 
-                
+                // https://stackoverflow.com/questions/30194562/json-parse-not-working
+                var messages = JSON.parse(JSON.parse(data));
                 var msgOutput = "";
-                for(var i = 0; i < messages.length; i++) {
-                    var msg = messages[i]; 
-                    
-                    msgOutput += "[" + msg.msgtime + "] " + msg.username + ": " + msg.msg + "\n";  // formatting output properly       
-                }       
-                
+                for (var i = 0; i < messages.length; i++) {
+                    var msg = messages[i];
+                    msgOutput += "[" + msg.msgtime + "] " + msg.username + ": " + msg.msg + "\n"; // formatting output properly       
+                }
+
                 document.getElementById("result").value = msgOutput; // displaying formatted output in text area
             })
             .catch(error => {
@@ -52,14 +73,13 @@ function getNew() {
 }
 
 setInterval(getNew, 300);
-
 var x = document.getElementById("msgBox");
 //taken from w3schools
-x.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    sendMsg();
-    x.value="";
-  }
+x.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        sendMsg();
+        x.value = "";
+    }
 });
 
