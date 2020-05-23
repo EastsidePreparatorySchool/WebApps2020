5e0c76a499de28d621575760e26074f01e133957
Kenneth Y <kyang@eastsideprep.org>
2020-05-22 18:38:32 -0700

    ChatServer Stuff

---
+++ ChatServer/target/classes/common.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function request(obj) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open(obj.verb || "GET", obj.url);
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        
+//        xhr.setRequestHeader("Access-Control-Allow-Origin","https://epsauth.azurewebsites.net");
+//        xhr.setRequestHeader("Access-Control-Allow-Origin","*");
+        
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(obj.body);
+    });
+}
+
+
+
