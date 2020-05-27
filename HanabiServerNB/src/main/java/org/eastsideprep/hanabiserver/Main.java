38000e7c0e265c9971122597d84f1f7548c8a2f4
EganTardif <57725052+EganTardif@users.noreply.github.com>
2020-05-27 12:31:41 -0700

    Deleted my update function
    Everest's function fills the role, deleted mine to aviod conflicts.

--- HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Main.java
+++ HanabiServerNB/src/main/java/org/eastsideprep/hanabiserver/Main.java
@@ -89,11 +89,8 @@ public class Main {
             return "/turn route";
         });
         
-        get("/update.lobby","application/json", (req,res)->{ //route specifically for sending lobby info to client
-            return games;                                    //so we don't have to send the arraylist of games 
-        }, new JSONRT());                                    //With each regular update for the game. 
-                                                             //Can be a separate update function that runs less frequently
-        gameControl = new GameControl();                     //Maybe even manually.
+                                       
+        gameControl = new GameControl();
     }
     
     public static void createGame() {
